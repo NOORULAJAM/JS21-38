5b3d0664ddacecb35623d9a68eff85d5e2c9f766
NOORULAJAM <66529861+NOORULAJAM@users.noreply.github.com>
2020-06-26 03:47:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,121 @@
+//  CHAPTER 20
+//  TASK 1
+// var firstname=prompt('ENTER FIRST NAME')
+// var lastname=prompt('ENTER LAST NAME')
+// document.write(firstname + lastname)
+
+//  TASK 2
+// var n=prompt('ENTER YOUR FAVORITE MOBILE ')
+// document.write("MY FAVORITE MOBILE IS =" + n +"<br>" )
+// document.write(" LENGTH IS =" + n.length)
+
+//  TASK 3
+// var str="pakistan"
+// var index=str.indexOf('n');
+// document.write("STRING = " + str +"<br>")
+// document.write("INDEX NO. IS = "+index)
+
+//  TASK 4
+// var str="HELLO WORLD"
+// var index=str.lastIndexOf('L');
+// document.write("STRING = " + str +"<br>")
+// document.write("INDEX NO. IS = "+index)
+
+//  TASK 5
+// var str="PAKISTANI"
+// var index=str.charAt(3)
+// document.write("STRING = " + str +"<br>")
+// document.write("INDEX = " + index )
+
+//  TASK 6
+// var firstname=prompt('ENTER FIRST NAME')
+// var lastname=prompt('ENTER LAST NAME')
+// document.write(firstname + lastname)
+
+// TASK 7
+// var str='HYDERABAD'
+// var replace=str.replace('HYDER','ISLAM')
+// document.write("STRING = " + str +"<br>")
+// document.write("REPLACE = " + replace )
+
+// TASK 8
+// var  message = 'Ali and Sami are best friends.They play cricket and football together'
+// var replace=message.replace('and','&').replace('and','&')
+// document.write('MESSAGE =' +message +'<br>')
+// document.write("REPLACE = " + replace )
+
+// TASK 9
+// var str="472"
+// var num=parseInt(str);
+// document.write('IN STRING ='+ typeof(str) +'<br>')
+// document.write('IN NUMBER ='+ typeof(num))
+
+// TASK 10
+// var str=prompt('WRITE PEANUTS:')
+// var upper=str.toUpperCase();
+// document.write('LOWER CASE = '+ str +'<br>')
+// document.write('UPPER CASE = '+ upper)
+
+// TASK 11
+// var str=prompt('WRITE JAVASCRIPT:')
+// var upper=(str.toUpperCase().charAt(0) + str.slice(1))
+// document.write('USER INPUT = '+ str +'<br>')
+// document.write('TITILE CASE = '+ upper)
+
+// TASK 12
+// var int=35.36;
+// var str=toString(int);                             not working
+// var remove=(int.toFixed() + int.split(2));
+// document.write('NUMBER ='+ int +'  ' + typeof(str) )
+// document.write('RESULT ='+ remove  )
+
+// TASK 13
+// var user = prompt('ENTER USER NAME')
+// if(user >= 'a' && user <='z' ||  user >= 'A' && user <='Z'  ||  user >= 0 && user <= 9   )
+// { 
+//     document.write(user)
+// }
+// else
+// {
+//     alert(' enter a valid username')
+// }
+
+//  TASK 14
+// var user=prompt('ENTER ITEM:')
+// user=user.toLowerCase();
+// var arr = ['cake', 'apple pie', 'cookie', 'chips', 'patties']
+
+// for(var i=0;i<=4 ;i++)
+// {
+// if (user === arr[i])
+// {
+//     alert('FOUND ITEM')
+//     document.write('INDEX NO. IS = '+arr.indexOf(user))
+// }
+// }
+
+// TASK 15
+// var pass=prompt('ENTER PASSWORD:')
+// if (pass.charAt(0) >= 0 || pass.charAt(0) <= 9)
+// {
+//     alert('PASSWORD CAN NOT BEGIN WITH A NUMBER' + '\n' +'ENTER VALID PASSWROD')
+// }
+// else
+// {
+//     document.write('WELCOME IN OUR WEBSITE')
+// }
+
+// TASK 16
+// var n="universityofkarachi"         not working
+// var arr=split(n,'\n')
+// document.write(arr)
+
+// TASK 17
+// var str = prompt('ENTER PAKISTAN :')
+// var last = str.slice(-1)
+// document.write('LAST CHARACTER IS = '+last)
+
+// TASK 18
+var str='The quick brown fox jumps over the lazy dog'
+var array=str.
+document.write(array)
